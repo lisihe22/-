05e712ab65f83586c4e044fd528a5705c40d6aba
Natalya <sorokanu95@mail.ru>
2023-01-16 20:49:09 +0500

    Задача 2 готово

---
+++ зд4/Program.cs
@@ -0,0 +1,28 @@
+﻿   Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+   Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+   Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+{ 
+if (a > b && a > c)
+Console.WriteLine(a);
+else if (a<b && b<c)
+Console.WriteLine (c);
+else if (a<b && b>c)
+Console.WriteLine (b);
+}
+//Console.WriteLine (c);
+//if (b > a )
+//Console.WriteLine(b);
+//else if (c > b )
+//Console.WriteLine(c);
+
+
+// if (c < b) 
+//Console.WriteLine(b);
+//if (c < a)
+//Console.WriteLine(a); 
+
+
