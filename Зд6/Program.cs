7bef8324bd98fb3b13fb26eca727c14cb24875fc
Natalya <sorokanu95@mail.ru>
2023-01-16 21:45:25 +0500

    Задача 6 гтово

---
+++ Зд6/Program.cs
@@ -0,0 +1,12 @@
+﻿   Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+{
+// double x = a/2;  
+//Console.WriteLine ("x=a/2=",(x)); //вывести на экран результат действия
+ //  Console.WriteLine (x);
+   if (a % 2==1)
+Console.WriteLine ("Нечетное");
+else
+Console.WriteLine ("Четное");
+}
