6d69bcf1a968b7af27b600f8e35945ae349b6f2f
Natalya <sorokanu95@mail.ru>
2023-01-20 19:40:22 +0500

    Задача 15 готово

---
+++ зд15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+   if(n >= 1 && n <= 5 )
+
+Console.Write("Будний");
+
+   else
+Console.Write("Выходной");
