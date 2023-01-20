fa5ffe3d3aa95ef48313d1c2db678b2472933d51
Natalya <sorokanu95@mail.ru>
2023-01-20 22:37:06 +0500

    Задача 10 готово

---
+++ зд10/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n/10%10;
+{
+    if (n<=999)
+    Console.WriteLine(m);
+    else
+    Console.WriteLine("Вы ввели не трехзначное число");
+
+}
+
+
+
+
