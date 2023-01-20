1015d2082b4a81e7d8365599ce46218ebfb4220f
Natalya <sorokanu95@mail.ru>
2023-01-21 00:48:57 +0500

    Задача 19 готово

---
+++ зд19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+string x = Console.ReadLine();
+int len = x.Length;
+
+if (len == 5)
+{
+if (x[0] == x[4] && x[1] == x[3])
+{
+ Console.WriteLine($"{x} - Палиндром");
+}
+else
+{
+ Console.WriteLine($"{x} - Не палиндром");
+}
+}
+else
+{
+Console.WriteLine($"Число {x} - не является пятизначным");
+}
