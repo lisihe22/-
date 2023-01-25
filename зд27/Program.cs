fed254858759871dac0039628ebbfad007bdcea4
Natalya <sorokanu95@mail.ru>
2023-01-25 12:21:33 +0500

    Задача 27

---
+++ зд27/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Введите число: ");
+string? a = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+  sum = sum + Convert.ToInt32(a[i].ToString());
+}
+Console.WriteLine($"Сумма цифр равна = {sum}");
