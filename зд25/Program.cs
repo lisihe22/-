3890c940b0e67fe228dd92c3cd772059d91ebd98
Natalya <sorokanu95@mail.ru>
2023-01-25 12:16:14 +0500

    Задача 25

---
+++ зд25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int st = a;
+
+    for (int i = 1; i < b; i++)
+{
+        st = st*a;
+}
+Console.WriteLine($"Число {a} в степень {b} = {st}");
